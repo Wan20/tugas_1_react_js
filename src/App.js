0f7ad7a3903b568ba842d90eed9c77a740f750dd
Wandy <wandy20number@gmail.com>
2020-09-24 14:14:43 +0700

    Tugas 1 React Js Niomic
    Commit Tugas 1 React Js Niomic

---
+++ src/App.js
@@ -0,0 +1,38 @@
+import React, { Component } from 'react';
+import MenuUtama from './Page/MenuUtama';
+import MenuProduct from './Page/MenuProduct';
+import MenuKontak from './Page/MenuKontak';
+import MenuTentangKami from './Page/MenuTentangKami';
+
+const Header = () => {
+  return (
+    <div>
+      <h4>Ini Halaman Untuk Header</h4>
+    </div>
+  )
+}
+
+const Footer = () => {
+  return (
+    <div>
+      <h4>Ini Halaman Untuk Footer</h4>
+    </div>
+  )
+}
+
+class App extends Component {
+ render() {
+    return (
+      <div>
+        <Header />
+        <MenuUtama />
+        <MenuProduct />
+        <MenuKontak />
+        <MenuTentangKami />
+        <Footer />
+      </div>
+    );
+ }
+}
+
+export default App;
